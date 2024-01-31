b1ac5ca7934f3426cacbbdfc88b09feb52e49e49
Daniel Kiss <littlesnorrboy@gmail.com>
2024-01-31 11:07:26 +0100

    read csv

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use leptos::logging::{error, log};
 use leptos::*;
+use leptos_use::use_event_listener;
+use web_sys::{
+    wasm_bindgen::{closure::Closure, JsCast},
+    FileReader,
+};
 
 fn main() {
     leptos::mount_to_body(App)
@@ -6,17 +12,38 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+    let (csv, set_csv) = create_signal("".to_owned());
+    let csv_input: NodeRef<html::Input> = create_node_ref();
+
+    let _ = use_event_listener(csv_input, ev::change, move |ev| {
+        let target = event_target::<web_sys::HtmlInputElement>(&ev);
+
+        if let Some(file) = target.files().and_then(|f| f.get(0)) {
+            let reader = FileReader::new().expect("Failed to create filereader");
+            let r = reader.clone();
+            let cb = Closure::wrap(Box::new(move |ev: web_sys::ProgressEvent| {
+                log!("Loaded: {:?}", ev.as_string());
+                log!("???? {:?}", r.result());
+                match r.result() {
+                    Ok(content) => {
+                        let content = content.as_string().unwrap();
+                        set_csv(content);
+                    }
+                    Err(err) => {
+                        error!("Failed to read file: {err:?}");
+                    }
+                }
+            }) as Box<dyn Fn(web_sys::ProgressEvent)>);
+
+            // TODO: handle errors
+            reader.read_as_text(&file).expect("Failed to read file");
+            reader.set_onload(Some(cb.as_ref().unchecked_ref()));
+            cb.forget();
+        }
+    });
 
     view! {
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }
-        >
-            "Click me: "
-            {count}
-        </button>
+        <p>CSV file: {csv}</p>
+        <input type="file" accept=".csv" placeholder="csv file" node_ref=csv_input/>
     }
 }
-
